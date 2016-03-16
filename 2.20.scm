7e4eb91518f03d64ae03eca2349fbf926b1fb7a9
zhangshiqiu <zhangshiqiu1993@126.com>
2016-03-16 00:56:18 -0400

    same-parity

---
+++ 2.20.scm
@@ -0,0 +1,14 @@
+#lang planet neil/sicp
+(define head car)
+(define tail cdr)
+(define (same-parity x . xs)
+  (let ((mark (remainder x 2)))
+    (define (iter xs a)
+      (cond ((null? xs) a)
+            ((= (remainder (head xs) 2) mark)
+             (iter (tail xs) (cons a (head xs))))
+            (else
+             (iter (tail xs) a))))
+    (iter xs x)))
+(same-parity 1 2 3 4 5 6 7)
+(same-parity 2 3 4 5 6 7)
