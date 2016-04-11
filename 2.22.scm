de6d2fa95bcc8df307e54bd09b2ca9ab329ba309
zhangshiqiu <zhangshiqiu1993@126.com>
2016-04-11 13:36:06 -0400

    square-list-2

---
+++ 2.22.scm
@@ -0,0 +1,20 @@
+#lang planet neil/sicp
+(define (square x) (* x x))
+
+;(define (square-list items)
+;  (if (null? items)
+;      nil
+;      (cons (square (car items))
+;            (square-list (cdr items)))))
+
+(define (square-list items)
+  (define (iter things answer)
+    (if (null? things)
+        answer
+        (iter (cdr things)
+              (cons answer (square (car things))))))
+  (iter items nil))
+
+;(define (square-list items) (map square items))
+
+(square-list (list 1 2 3 4))
