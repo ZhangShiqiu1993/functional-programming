65607c31725e714667c0632651e4bf686e2cd02d
ZhangShiqiu1993 <zhangshiqiu1993@gmail.com>
2019-02-13 01:00:07 -0800

    funny-number-stream

--- pl2/week1/hw4/hw4.rkt
+++ pl2/week1/hw4/hw4.rkt
@@ -25,3 +25,10 @@
 (define (stream-for-n-steps s n)
   (cond [(= n 0) null]
         [#t (cons (car (s)) (stream-for-n-steps (cdr (s)) (- n 1)))]))
+
+;; 5
+(define funny-number-stream
+  (letrec ([f (lambda (x)
+                (cons (if (= 0 (remainder x 5)) (- x) x)
+                      (lambda () (f (+ x 1)))))])
+    (lambda () (f 1))))
