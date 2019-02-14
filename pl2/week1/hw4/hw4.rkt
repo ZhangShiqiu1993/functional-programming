99c437665189d94126ae2c3c8a13f72a7d753705
ZhangShiqiu1993 <zhangshiqiu1993@gmail.com>
2019-02-14 10:45:02 -0800

    while-less

--- pl2/week1/hw4/hw4.rkt
+++ pl2/week1/hw4/hw4.rkt
@@ -78,3 +78,15 @@
                                   0
                                   (+ next-to-replace 1)))
                         ans)))))))
+
+;; 11
+(define-syntax while-less
+  (syntax-rules (do)
+    ((while-less x do y)
+      (let ([z x])
+        (letrec ([loop (lambda ()
+			                  (let ([w y])
+		 	                    (if (or (not (number? w)) (>= w z))
+			                        #t
+			                        (loop))))])
+          (loop))))))
