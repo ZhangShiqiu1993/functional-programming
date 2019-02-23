04994b1b099c753d949d21ba6cfaef9d3e966db8
ZhangShiqiu1993 <zhangshiqiu1993@gmail.com>
2019-02-23 11:53:11 -0800

    Update hw6assignment.rb

--- pl3/week1/hw6/hw6assignment.rb
+++ pl3/week1/hw6/hw6assignment.rb
@@ -5,7 +5,12 @@
 
 class MyPiece < Piece
   # The constant All_My_Pieces should be declared here
-
+  All_My_Pieces = All_Pieces + [
+      [[[0, 0], [-1, 0], [1, 0], [2, 0], [-2,0]], # 5-long
+       [[0, 0], [0, -1], [0, 1], [0, 2], [0, -2]]],
+      rotations([[0, 0], [-1, 0], [1, 0], [0, -1], [-1,-1]]), # utah
+      rotations([[0, 0], [1, 0], [0, 1]]) # short-L
+  ]
   # your enhancements here
 
 end
